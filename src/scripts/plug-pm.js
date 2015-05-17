6e507dfe9f6c273790b348fde7ed6eafd9d14470
jbrowne <jbrowne@cs.ucsb.edu>
2015-05-17 13:47:01 -0700

    Removed "self = this" pattern.

--- src/scripts/plug-pm.js
+++ src/scripts/plug-pm.js
@@ -1,10 +1,34 @@
+/*
+The MIT License (MIT)
+Copyright (c) 2015 Jeff Browne
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 (function() {
-    var self = this;
     var info = {
-        version: 0.5,
+        version: 0.6,
         author: "citizenSnips",
-    }
-    var Chat = function (srcId, srcName, dstId, dstName, msg) {
+    };
+
+    function Chat(srcId, srcName, dstId, dstName, msg) 
+    {
         var chat = {
             message : msg,
             recipient : 
@@ -22,13 +46,13 @@
         return chat;
     };
 
-    self.FirebaseData = {
+    var FirebaseData = {
         URL : "https://plug-pm.firebaseio.com",
         ReceiveChannel : null,
         SendChannel : null
     };
 
-    self.displayReceivedChat = function(chat) {
+    function displayReceivedChat(chat) {
         var chatEl = 
           jQuery("<div>", {class: "message user-action"})
           .append(
@@ -53,13 +77,13 @@
 
     };
 
-    self.displayStatusChat = function(message)
+    function displayStatusChat(message)
     {
         var statusChat = Chat(-1, "plug.pm", -1, "", message);
-        self.displayReceivedChat(statusChat);
+        displayReceivedChat(statusChat);
     };
 
-    self.displaySentChat = function(chat) {
+    function displaySentChat(chat) {
         var chatEl = 
           jQuery("<div>", {class: "message user-action"})
           .append(
@@ -83,20 +107,20 @@
         $("#chat-messages").append(chatEl);  
     };
 
-    self.onChatRecieved = function(chat) 
+    function onChatRecieved(chat) 
     {
 
-        self.displayReceivedChat(chat);
+        displayReceivedChat(chat);
     };
 
-    self.sendChatToUserInRoom = function(username, message) 
+    function sendChatToUserInRoom(username, message) 
     {
         var success = false;
         var userList = API.getUsers();
         for (var idx in userList) {
             if (userList[idx].username == username) {
                 var id = userList[idx].id;
-                self.sendChat_(id, username, message);
+                sendChat_(id, username, message);
                 success = true;
                 break;
             }
@@ -104,18 +128,18 @@
         return success;
     };
 
-    self.onChatCommand = function(command) {
+    function onChatCommand(command) {
         var idx = command.search("/pm");
         if (idx == 0) {
             var nameAndMessage = command.slice(4);
             if (nameAndMessage[0] == "@") {
                 nameAndMessage = nameAndMessage.slice(1);
             }
-            var nameAndMessageTuple = self.splitUsernameMessage_(nameAndMessage);
+            var nameAndMessageTuple = splitUsernameMessage_(nameAndMessage);
             var name = nameAndMessageTuple[0];
             var message = nameAndMessageTuple[1];
             console.log(name + ":" + message);
-            var result = self.sendChatToUserInRoom(name, message);
+            var result = sendChatToUserInRoom(name, message);
             if (!result) {
                 API.chatLog("PM failed...");
                 console.log("\"" + command + "\"" + " == " + name + ":" + message);
@@ -123,25 +147,25 @@
         }
     };
 
-    self.onChatMessagesReceived_ = function(snapshot) 
+    function onChatMessagesReceived_(snapshot) 
     {
         var chatMessages = snapshot.val();
 
         for (var idx in chatMessages) {
             var chat = chatMessages[idx];
-            self.onChatRecieved(chat);
+            onChatRecieved(chat);
         }       
         FirebaseData.ReceiveChannel.remove();
     };
 
-    self.sendChat_ = function(dstId, dstName, message) 
+    function sendChat_(dstId, dstName, message) 
     {
         var chat = Chat(API.getUser().id, API.getUser().username, dstId, dstName, message);
-        self.displaySentChat(chat);
+        displaySentChat(chat);
         FirebaseData.SendChannel.child(chat.recipient.id).push(chat);
     };
 
-    self.splitUsernameMessage_ = function(input) 
+    function splitUsernameMessage_(input) 
     {
         var usersInRoom = API.getUsers();
         var username = "";
@@ -156,34 +180,32 @@
         return ["",""];
     }
 
-    self.setup = function() 
+    function setup() 
     {
         if (typeof API == "undefined" || !API.enabled) {
-            setTimeout (self.setup, 500);
+            setTimeout (setup, 500);
             return;
         }
 
         var present = "id" in API.getUser();
         FirebaseData.SendChannel = new Firebase(FirebaseData.URL + "/UserChannels/");
         FirebaseData.ReceiveChannel = FirebaseData.SendChannel.child(API.getUser().id);
-        FirebaseData.ReceiveChannel.on("value", function(snapshot) {self.onChatMessagesReceived_(snapshot);});
-        API.on(API.CHAT_COMMAND, self.onChatCommand);
+        FirebaseData.ReceiveChannel.on("value", function(snapshot) {onChatMessagesReceived_(snapshot);});
+        API.on(API.CHAT_COMMAND, onChatCommand);
         API.chatLog("Loaded Plug.pm " + info.version);
-        self.displayStatusChat("Type /pm @person <message>");
-        self.displayStatusChat("What's new: You can pm people with spaces in their names!");
-
+        displayStatusChat("Type /pm @person <message>");
+        displayStatusChat("What's new: You can pm people with spaces in their names!");
     };
 
-    self.preload = function() 
+    function preload() 
     {
         if (typeof $ == 'undefined') {
-            setTimeout (self.preload, 500);
+            setTimeout (preload, 500);
             return;
         }
-        $.getScript("https://cdn.firebase.com/js/client/1.1.0/firebase.js", self.setup);        
+        $.getScript("https://cdn.firebase.com/js/client/1.1.0/firebase.js", setup);        
     };
 
     preload();
-    window.chat = self;
-}).call(self);
+}).call(this);
 
